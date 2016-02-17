05a17281c8b979000d172de8539294118e09a3c3
Zachary Scott <e@zzak.io>
2016-02-17 19:02:07 +0900

    Disable manual assert bytecode compliation, and update the symbol name.
    We're trying to link the mruby-bin-test rbfiles here instead.

--- lib/mruby-build/tasks/mrbtest.rb
+++ lib/mruby-build/tasks/mrbtest.rb
@@ -22,17 +22,17 @@ module MRuby
               mrbtest_lib = libfile("#{mrbtest.build_dir}/mrbtest")
               mrbtest_objs = []
 
-              assert_c = "#{mrbtest.build_dir}/assert.c"
-              assert_rb = "#{mrbtest.dir}/lib/assert.rb"
-              assert_lib = assert_c.ext(exts.object)
-              mrbtest_objs << assert_lib
+              #assert_c = "#{mrbtest.build_dir}/assert.c"
+              #assert_rb = "#{mrbtest.dir}/lib/assert.rb"
+              #assert_lib = assert_c.ext(exts.object)
+              #mrbtest_objs << assert_lib
 
-              file assert_lib => assert_c
-              file assert_c => assert_rb do |t|
-                open(t.name, 'w') do |f|
-                  mrbc.run f, assert_rb, 'mrbtest_assert_irep'
-                end
-              end
+              #file assert_lib => assert_c
+              #file assert_c => assert_rb do |t|
+              #  open(t.name, 'w') do |f|
+              #    mrbc.run f, assert_rb, 'mrbtest_assert_irep'
+              #  end
+              #end
 
               gem_table = gems.generate_gem_table self
 
@@ -46,9 +46,9 @@ module MRuby
                   FileUtils.mkdir_p File.dirname(t.name)
                   open(t.name, 'w') do |f|
                     g.print_gem_test_header(f)
-                    test_preload = g.test_preload and [g.dir, MRUBY_ROOT].map {|dir|
-                      File.expand_path(g.test_preload, dir)
-                    }.find {|file| File.exist?(file) }
+                    #test_preload = g.test_preload and [g.dir, MRUBY_ROOT].map {|dir|
+                    #  File.expand_path(g.test_preload, dir)
+                    #}.find {|file| File.exist?(file) }
 
                     f.puts %Q[/*]
                     f.puts %Q[ * This file contains a test code for #{g.name} gem.]
@@ -57,11 +57,11 @@ module MRuby
                     f.puts %Q[ *   This file was generated!]
                     f.puts %Q[ *   All manual changes will get lost.]
                     f.puts %Q[ */]
-                    if test_preload.nil?
-                      f.puts %Q[extern const uint8_t mrbtest_assert_irep[];]
-                    else
-                      g.build.mrbc.run f, test_preload, "gem_test_irep_#{g.funcname}_preload"
-                    end
+                    #if test_preload.nil?
+                      f.puts %Q[extern const uint8_t gem_mrblib_irep_mruby_test[];]
+                    #else
+                    #  g.build.mrbc.run f, test_preload, "gem_test_irep_#{g.funcname}_preload"
+                    #end
                     g.test_rbfiles.flatten.each_with_index do |rbfile, i|
                       g.build.mrbc.run f, rbfile, "gem_test_irep_#{g.funcname}_#{i}"
                     end
@@ -91,11 +91,11 @@ module MRuby
                           f.puts %Q[  mrb_state_atexit(mrb2, GENERATED_TMP_mrb_#{d.funcname}_gem_final);]
                         end
                         f.puts %Q[  mrb_init_test_driver(mrb2, mrb_test(mrb_gv_get(mrb, mrb_intern_lit(mrb, "$mrbtest_verbose"))));]
-                        if test_preload.nil?
-                          f.puts %Q[  mrb_load_irep(mrb2, mrbtest_assert_irep);]
-                        else
-                          f.puts %Q[  mrb_load_irep(mrb2, gem_test_irep_#{g.funcname}_preload);]
-                        end
+                        #if test_preload.nil?
+                          f.puts %Q[  mrb_load_irep(mrb2, gem_mrblib_irep_mruby_test);]
+                        #else
+                        #  f.puts %Q[  mrb_load_irep(mrb2, gem_test_irep_#{g.funcname}_preload);]
+                        #end
                         f.puts %Q[  if (mrb2->exc) {]
                         f.puts %Q[    mrb_print_error(mrb2);]
                         f.puts %Q[    exit(EXIT_FAILURE);]
